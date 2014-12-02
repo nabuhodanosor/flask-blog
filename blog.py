eb8a444198c4a277ba41b63b1bb379787842f178
Yevhen Grinman <yevgirnman@yahoo.com>
2014-12-02 16:33:47 -0500

    Add login page, add main page functionality, add main.html page

--- blog.py
+++ blog.py
@@ -3,12 +3,19 @@
 # imports
 from flask import Flask, render_template, request, session, \
 	flash, redirect, url_for, g
+from functools import wraps
 import sqlite3
+import os
 
 #configuration
 DATABASE = 'blog.db'
 
 app = Flask(__name__)
+
+USERNAME = 'admin'
+PASSWORD = 'password'
+SECRET_KEY = os.urandom(24)
+
 
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
@@ -17,13 +24,42 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or \
+				request.form['password'] != app.config['PASSWORD']:
+			error = "Invalid Credentials. Please, try again."
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 @app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
 
 if __name__ == '__main__':
 	app.run(debug=True)
